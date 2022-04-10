c18225a723d58ed53659f25518efd3ea1d1d244e
Kirill Sayed <sayed.kirill@gmail.com>
2022-04-10 10:50:42 +0300

    Basic data in Studio.cs

--- lab2/Studio/Studio.cs
+++ lab2/Studio/Studio.cs
@@ -1,6 +1,25 @@
 ﻿namespace lab2.Studio;
 
-public class Studio
+public sealed class Studio
 {
-    
+    private List<Coder.Coder> _coders;
+    private List<Designer.Designer> _designers;
+
+    private static Studio _instance;
+
+    private Studio()
+    {
+        
+    }
+
+    public Studio getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Studio();
+        }
+
+        return _instance;
+    }
+
 }
