b6a72c147e291ee9f3fbee2b747f223e33255a56
Kirill Sayed <sayed.kirill@gmail.com>
2022-04-11 10:10:27 +0300

    Redone project manager's tasks

--- lab2/ProjectManager/ProjectManagerEntity.cs
+++ lab2/ProjectManager/ProjectManagerEntity.cs
@@ -1,14 +1,27 @@
 ﻿using lab2.ProjectManager.Tasks;
+using lab2.Studio;
 
 namespace lab2.ProjectManager;
 
-public class ProjectManagerEntity
+public class ProjectManagerEntity : EntityManagement
 {
-    public WorkerTask CurrentWorkerTask { get; set; }
+    public List<WorkerTask> projectManagerTasks { get; set; }
 
     public ProjectManagerEntity()
     {
-        CurrentWorkerTask = AnalyzePerformance.getInstance();
+        projectManagerTasks = new List<WorkerTask>();
+        projectManagerTasks.Add(new AnalyzePerformance());
+        projectManagerTasks.Add(new AnalyzePerformance("Analyze designer performance", 20));
+        projectManagerTasks.Add(new AnalyzePerformance("Analyze group performance", 25));
     }
 
+    public void addTask(WorkerTask task)
+    {
+        projectManagerTasks.Add(task);
+    }
+
+    public void addTask(string description, int timeRequired)
+    {
+        projectManagerTasks.Add(new AnalyzePerformance(description, timeRequired));
+    }
 }
